8591f2b055ecedbf18e7e46d0fb052f1d3446e74
aditikocherlakota <k.aditi101@gmail.com>
2017-07-11 13:04:45 -0700

    Update OutputUnique.java
    corrected barcode scanner- checked against manual version

--- OutputUnique.java
+++ OutputUnique.java
@@ -1,16 +1,11 @@
-//aditi kocherlakota corrected barcode scanner 7/05/17
+//aditi kocherlakota corrected barcode scanner July 11 2017
 import java.io.*;
 import java.util.*;
 import java.util.Scanner;
 
 class OutputUnique {
-    public static void main(String args[]) throws Exception {
-        String barcode="";
-        for (String x: args) {
-            String delimiter = " ";
-            String[] temp = x.split(delimiter);
-        }
 
+    public static void main(String args[]) throws Exception {
         Scanner scan= new Scanner(System.in);
          System.out.print("what is the directory to your run folder?\n");
          String dir1 = scan.next();
@@ -18,23 +13,12 @@ class OutputUnique {
          String forward = scan.next();
           System.out.print("What is the name of your reverse file?\n");
          String reverse = scan.next();
-           System.out.print("What would you like to label your demultiplexed files with?\n");
-           String label = scan.next();
-           System.out.print("What is the name of your .txt file with your barcodes?\n");
-           String barcodeName = scan.next();
-           System.out.print("how many barcodes do you have?\n");
+        System.out.print("What would you like to label your demultiplexed files with?\n");
+       String label = scan.next();
+      System.out.print("What is the name of your .txt file with your barcodes?\n");
+      String barcodeName = scan.next();
+      System.out.print("how many barcodes do you have?\n");
        int numBarcode = scan.nextInt();
-        String trueseq = "";
-        String readid = "";
-        String barcoderef = "";
-        String umi = "";
-        String read = "";
-        String qual = "";
-        String strand = "";
-        String sequence = "";
-        Map countdict = new HashMap();
-        Map newseqdict = new HashMap();
-        Set indices = new HashSet();
         String[] barcodeList;
         barcodeList = new String[numBarcode];
         File barFile = new File(barcodeName);
@@ -42,7 +26,18 @@ class OutputUnique {
           for(int i=0; i<numBarcode; i++)
           {
             barcodeList[i] = scBar.nextLine();
-          }
+            System.out.print(barcodeList[i] + " \n");
+            String trueseq = "";
+            String readid = "";
+            String barcoderef = "";
+            String umi = "";
+            String read = "";
+            String qual = "";
+            String strand = "";
+            String sequence = "";
+            Map countdict = new HashMap();
+            Map newseqdict = new HashMap();
+            Set indices = new HashSet();
         FileReader fr1 = new FileReader(dir1 + forward);
         FileReader fr2 = new FileReader(dir1 + reverse);
         BufferedReader br1 = new BufferedReader(fr1);
@@ -51,8 +46,6 @@ class OutputUnique {
         int position = 0;
         int counter=0;
         int thresh = 75;
-        for (int d=0; d<numBarcode; d++)
-        {
         while(true) {
             String s1 = br1.readLine();
             String s2 = br2.readLine();
@@ -80,15 +73,15 @@ class OutputUnique {
             if (s2.length() > thresh)
                 {
                  qual = s2.substring(5,thresh);
-                if (barcoderef.equals(barcodeList[d]))
+                if (barcoderef.equals(barcodeList[i]))
                     {
                      newseqdict.put(read, readid + "\n" + trueseq.substring(5,thresh) + "\n" + "+" + "\n" + qual + "\n");
                     }
                 }
             }
            counter = counter + 1;
-        }
-FileWriter f0 = new FileWriter(dir1 + label + barcodeList[d] + "_nodupe_trimmed.fastq");
+         }
+FileWriter f0 = new FileWriter(dir1 + label + barcodeList[i] + "_nodupe_trimmed.fastq");
 Set entries = newseqdict.entrySet();
 Iterator iterator = entries.iterator();
 while(iterator.hasNext()) {
@@ -96,9 +89,9 @@ while(iterator.hasNext()) {
     String fastq = (String)entry.getValue();
     f0.write(fastq);
 }
-f0.close();
-}
 fr1.close();
 fr2.close();
+f0.close();
+}
 }
 }
